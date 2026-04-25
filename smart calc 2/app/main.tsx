5ba1927bb77923b816b2ea96093a2c0f451f80a8
yussef2009 <yussef.essamabdallah@gmail.com>
2026-04-25 20:23:46 +0300

    feat: initialize project structure with Recharts integration and main application entry point

--- smart calc 2/app/main.tsx
+++ smart calc 2/app/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '../styles/index.css'
 import { AuthProvider } from './lib/AuthContext'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <SpeedInsights />
     </AuthProvider>
   </React.StrictMode>,
 )
